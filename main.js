5e1dcc61fc8a9f9461e8addc554c627abe49430c
Ali Gangji <ali@neonrain.com>
2014-04-02 16:29:02 -0600

    dbootstrap isolated and converted to less for use on top of bootstrap

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+    :copyright: Copyright 2012 Martin Pengelly-Phillips
+    :license: See LICENSE.txt.
+*/
+//'xstyle/css!./theme/dbootstrap/dbootstrap.css',
+define([
+    './icon_support'
+],
+
+function (TemplatedMixin) {
+	return {};
+	/*
+    return {
+        'TemplatedMixin': TemplatedMixin
+    };
+    */
+});
+
